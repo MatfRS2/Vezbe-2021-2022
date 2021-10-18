89448ec65e2b4503b6c32ab75253aa9544bf3f8b
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-10-18 15:17:53 +0200

    Fixed internal datatype for product id

--- Webstore/Services/Catalog/Catalog.API/Entities/Product.cs
+++ Webstore/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ namespace Catalog.API.Entities
 {
     public class Product
     {
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
