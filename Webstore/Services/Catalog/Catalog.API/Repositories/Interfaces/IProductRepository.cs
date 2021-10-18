d961e8b261527e83c970554bfd2f9cc164326339
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-10-18 14:22:49 +0200

    Added a product repository with get products method

---
+++ Webstore/Services/Catalog/Catalog.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+    }
+}
---
+++ Webstore/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.Find(p => true).ToListAsync();
+        }
+    }
+}
