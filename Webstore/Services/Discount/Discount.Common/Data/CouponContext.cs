adeabf67183412fee03a974b95324a6598b6e742
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-10-25 19:18:24 +0200

    Added a common class library for Discount microservices with entities

---
+++ Webstore/Services/Discount/Discount.Common/Data/CouponContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.Common.Data
+{
+    public class CouponContext : ICouponContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public CouponContext(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+    }
+}
