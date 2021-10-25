f342434442f24a3d71c7eb6801db55a4df4ab5b0
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-10-25 19:19:02 +0200

    Added DTOs for Discount project

---
+++ Webstore/Services/Discount/Discount.Common/DTOs/BaseCouponDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.Common.DTOs
+{
+    public class BaseCouponDTO
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
