a497e2fe0bfd4a4dbdba473a8c22923d202fe3a8
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-10-25 19:19:52 +0200

    Started working on coupon repository
    Added the interface and get discount by product name operation w/ postgresql db and dapper orm

---
+++ Webstore/Services/Discount/Discount.Common/Repositories/ICouponRepository.cs
@@ -0,0 +1,18 @@
+﻿using Discount.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.Common.Repositories
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDTO> GetDiscount(string productName);
+        Task<bool> CreateDiscount(CreateCouponDTO couponDTO);
+        Task<bool> UpdateDiscount(UpdateCouponDTO couponDTO);
+        Task<bool> DeleteDiscount(string productName);
+        Task<IEnumerable<CouponDTO>> GetRandomDiscounts(int numberOfDiscounts);
+    }
+}
