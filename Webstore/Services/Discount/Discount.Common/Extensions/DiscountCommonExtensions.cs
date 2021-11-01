f966acd1245f5e28edd76ffc976e95c1131ffb51
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-11-01 14:23:10 +0100

    Finished coupon repository and added DI

---
+++ Webstore/Services/Discount/Discount.Common/Extensions/DiscountCommonExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Discount.Common.Data;
+using Discount.Common.DTOs;
+using Discount.Common.Entities;
+using Discount.Common.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.Common.Extensions
+{
+    public static class DiscountCommonExtensions
+    {
+        public static void AddDiscountCommonServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICouponContext, CouponContext>();
+            services.AddScoped<ICouponRepository, CouponRepository>();
+            services.AddAutoMapper(configuration =>
+            {
+                configuration.CreateMap<CouponDTO, Coupon>().ReverseMap();
+            });
+        }
+    }
+}
