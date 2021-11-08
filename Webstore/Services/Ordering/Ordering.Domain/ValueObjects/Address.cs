0e225ef655db037e396c87670729ddc7a08aa41e
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-11-08 19:08:27 +0100

    Domain layer implemented for Ordering
    Closes #6

---
+++ Webstore/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -0,0 +1,39 @@
+﻿using Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.ValueObjects
+{
+    public class Address : ValueObject
+    {
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string Country { get; private set; }
+        public string ZipCode { get; private set; }
+        public string EmailAddress { get; private set; }
+
+        public Address(string street, string city, string state, string country, string zipCode, string emailAddress)
+        {
+            Street = street ?? throw new ArgumentNullException(nameof(street));
+            City = city ?? throw new ArgumentNullException(nameof(city));
+            State = state ?? throw new ArgumentNullException(nameof(state));
+            Country = country ?? throw new ArgumentNullException(nameof(country));
+            ZipCode = zipCode ?? throw new ArgumentNullException(nameof(zipCode));
+            EmailAddress = emailAddress ?? throw new ArgumentNullException(nameof(emailAddress));
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Street;
+            yield return City;
+            yield return State;
+            yield return Country;
+            yield return ZipCode;
+            yield return EmailAddress;
+        }
+    }
+}
