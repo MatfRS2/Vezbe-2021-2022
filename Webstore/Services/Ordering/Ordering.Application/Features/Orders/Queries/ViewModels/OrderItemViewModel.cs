07ccd13b1592de9d07f45cc411fc6dd1b54a9007
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-11-15 14:32:41 +0100

    Added the list orders query and create order command

---
+++ Webstore/Services/Ordering/Ordering.Application/Features/Orders/Queries/ViewModels/OrderItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.ViewModels
+{
+    public class OrderItemViewModel
+    {
+        // Relevant information from EntityBase
+        public int Id { get; set; }
+
+        // Relevant information from OrderItem
+        public string ProductName { get; set; }
+        public string ProductId { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Units { get; set; }
+    }
+}
