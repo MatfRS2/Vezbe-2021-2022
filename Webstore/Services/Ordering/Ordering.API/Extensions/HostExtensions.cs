212585b2103683747699e850195fa8e2d4b31656
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-12-06 14:38:58 +0100

    Added Order DB migrations

---
+++ Webstore/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Polly;
+
+namespace Ordering.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
+
+                    var retry = Policy.Handle<SqlException>()
+                        .WaitAndRetry(
+                            retryCount: 5,
+                            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                            onRetry: (exception, retryCount, context) =>
+                            {
+                                logger.LogError($"Retry {retryCount} if {context.PolicyKey} at {context.OperationKey}, due to {exception}.");
+                            });
+                    retry.Execute(() => InvokeSeeder(seeder, context, services));
+
+                    logger.LogInformation("Migrating database associated with context {DbContextName} was successful", typeof(TContext).Name);
+                } 
+                catch (SqlException e)
+                {
+                    logger.LogError(e, "An error occured while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+                }
+            }
+
+            return host;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+}
