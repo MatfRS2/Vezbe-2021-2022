b70b25e674bcb56437859461c31591b4e5119043
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-12-13 14:40:28 +0100

    Added users and roles

---
+++ Webstore/Security/IdentityServer/Extensions/IdentityExtensions.cs
@@ -0,0 +1,43 @@
+﻿using IdentityServer.Data;
+using IdentityServer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static IServiceCollection ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("IdentityConnectionString"));
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<ApplicationContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
---
+++ Webstore/Security/IdentityServer/Migrations/20211213133822_AddedRolesToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Migrations
+{
+    public partial class AddedRolesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "23f4e8c4-f5f2-44a5-86e0-0a287d8c4989", "8e560fab-b695-487e-b42d-ffba4bd2489e", "Buyer", "BUYER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "e0524ae4-2126-4987-9a77-fb3fcdaec257", "4413b030-23c5-434e-aa54-5d24d90bfa0b", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "23f4e8c4-f5f2-44a5-86e0-0a287d8c4989");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e0524ae4-2126-4987-9a77-fb3fcdaec257");
+        }
+    }
+}
