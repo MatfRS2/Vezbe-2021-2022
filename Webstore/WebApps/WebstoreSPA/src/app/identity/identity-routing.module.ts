8e0ff75e00be2c36e2c568c4656ecf752b30e38c
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2021-12-27 15:26:03 +0100

    Added bootstrap and login form component

--- Webstore/WebApps/WebstoreSPA/src/app/identity/identity-routing.module.ts
+++ Webstore/WebApps/WebstoreSPA/src/app/identity/identity-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './feature-authentication/login-form/login-form.component';
 import { IdentityComponent } from './identity.component';
 
-const routes: Routes = [{ path: '', component: IdentityComponent }];
+// Podrazumeva se prefix /identity
+const routes: Routes = [
+  { path: '', component: IdentityComponent, children: [
+    { path: 'login', component: LoginFormComponent }
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- Webstore/WebApps/WebstoreSPA/src/app/identity/identity.module.ts
+++ Webstore/WebApps/WebstoreSPA/src/app/identity/identity.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { IdentityRoutingModule } from './identity-routing.module';
 import { IdentityComponent } from './identity.component';
+import { LoginFormComponent } from './feature-authentication/login-form/login-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    IdentityComponent
+    IdentityComponent,
+    LoginFormComponent
   ],
   imports: [
     CommonModule,
-    IdentityRoutingModule
+    IdentityRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class IdentityModule { }
