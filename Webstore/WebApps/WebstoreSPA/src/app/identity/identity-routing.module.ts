91fca15948ba529389654a89a9c6677ced1ee772
Nikola Ajzenhamer <ajzenhamernikola@gmail.com>
2022-01-15 14:00:27 +0100

    [Angular] Added guards

--- Webstore/WebApps/WebstoreSPA/src/app/identity/identity-routing.module.ts
+++ Webstore/WebApps/WebstoreSPA/src/app/identity/identity-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '../shared/guards/not-authenticated.guard';
 import { LoginFormComponent } from './feature-authentication/login-form/login-form.component';
 import { LogoutComponent } from './feature-authentication/logout/logout.component';
 import { UserProfileComponent } from './feature-user-info/user-profile/user-profile.component';
@@ -8,7 +9,7 @@ import { IdentityComponent } from './identity.component';
 // Podrazumeva se prefix /identity
 const routes: Routes = [
   { path: '', component: IdentityComponent, children: [{ path: 'login', component: LoginFormComponent }] },
-  { path: 'profile', component: UserProfileComponent },
+  { path: 'profile', component: UserProfileComponent, canActivate: [NotAuthenticatedGuard] },
   { path: 'logout', component: LogoutComponent },
 ];
 
